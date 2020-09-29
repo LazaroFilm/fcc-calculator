ce195cc82fd14dcfa59948390fd73338fdac5a92
Victor Lazaro <lazarofilm@gmail.com>
2020-09-29 13:26:33 -0400

    Updates

---
+++ precache-manifest.9ff3918210b0525faae68745a2b5fb36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ee7ef874f5e074037e3c65d66cadc2",
+    "url": "/fcc-calculator/index.html"
+  },
+  {
+    "revision": "66fa445faa5da6f5735e",
+    "url": "/fcc-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f10fba48f7fe0c2f2ac",
+    "url": "/fcc-calculator/static/js/2.41f1c41f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-calculator/static/js/2.41f1c41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66fa445faa5da6f5735e",
+    "url": "/fcc-calculator/static/js/main.5155c177.chunk.js"
+  },
+  {
+    "revision": "7281f81b1ccd46a2aea4",
+    "url": "/fcc-calculator/static/js/runtime-main.ea8d039d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fcc-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
