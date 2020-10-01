3ccf8bb6cd9999ba369a03024a691c7a912b7bca
Victor Lazaro <lazarofilm@gmail.com>
2020-10-01 12:09:00 -0400

    Styling the keypad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { evaluate, format } from "mathjs";
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
 import Keypad from "./Keypad";
 
 function App() {
@@ -21,23 +21,22 @@ function App() {
     <div className="App">
       <Keypad />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>This will become a calculator... at some point.</p>
         <p>this is displaying: {display}</p>
         <button onClick={() => setDisplay("updated display")}>Click me!</button>
         <button onClick={() => setDisplay("initial display")}>Reset</button>
         <p>{3 + 5 * 6 - 2 / 4}</p>
-
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={calc}
-            onChange={(e) => setCalc(e.target.value)}
-          />
-          <input type="submit" value="=" />
-        </form>
-        <p>{format(evaluate(result), { precision: 14 })}</p>
       </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={calc}
+          onChange={(e) => setCalc(e.target.value)}
+        />
+        <input type="submit" value="=" />
+      </form>
+      <p>{format(evaluate(result), { precision: 14 })}</p>
     </div>
   );
 }
--- src/Keypad.js
+++ src/Keypad.js
@@ -1,37 +1,204 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import "./Keypad.css";
 
 function Keypad() {
   return (
     <>
-      <div>
-        <button>C</button>
-        <button>/</button>
-        <button>*</button>
-        <button>b</button>
+      <div
+        className="btn-group-vertical ml-5 mt-5"
+        role="group"
+        aria-label="Basic example"
+      >
+        {/* <input className="text-center form-control-lg mb-2" id="code" /> */}
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            C
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            /
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            *
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            b
+          </button>
+        </div>
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            1
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            2
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            3
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            -
+          </button>
+        </div>
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            4
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            5
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            6
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            +
+          </button>
+        </div>
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            7
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            8
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            9
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            =
+          </button>
+        </div>
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom2"
+          >
+            0
+          </button>
+          {/* <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            0
+          </button> */}
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            .
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-outline-light py-3 btn-custom"
+          >
+            =
+          </button>
+        </div>
       </div>
-      <div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>-</button>
-      </div>
-      <div>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-      </div>
-      <div>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>=</button>
-      </div>
-      <div>
-        <button>0</button>
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
+      <br />
+
+      <div
+        className="btn-group-vertical ml-4 mt-4"
+        role="group"
+        aria-label="Basic example"
+      >
+        <div className="btn-group">
+          <input className="text-center form-control-lg mb-2" id="code" />
+        </div>
+        <div className="btn-group">
+          <button type="button" className="btn btn-outline-secondary py-3">
+            1
+          </button>
+          <button type="button" className="btn btn-outline-secondary py-3">
+            2
+          </button>
+          <button type="button" className="btn btn-outline-secondary py-3">
+            3
+          </button>
+        </div>
+        <div className="btn-group">
+          <button type="button" className="btn btn-outline-secondary py-3">
+            4
+          </button>
+          <button type="button" className="btn btn-outline-secondary py-3">
+            5
+          </button>
+          <button type="button" className="btn btn-outline-secondary py-3">
+            6
+          </button>
+        </div>
+        <div className="btn-group">
+          <button type="button" className="btn btn-outline-secondary py-3">
+            7
+          </button>
+          <button type="button" className="btn btn-outline-secondary py-3">
+            8
+          </button>
+          <button type="button" className="btn btn-outline-secondary py-3">
+            9
+          </button>
+          <button type="button" className="btn btn-outline-secondary py-3">
+            9
+          </button>
+        </div>
+        <div className="btn-group">
+          <button type="button" className="btn btn-outline-secondary py-3">
+            &lt;
+          </button>
+          <button type="button" className="btn btn-outline-secondary py-3">
+            0
+          </button>
+          <button type="button" className="btn btn-primary py-3">
+            Go
+          </button>
+        </div>
       </div>
     </>
   );
