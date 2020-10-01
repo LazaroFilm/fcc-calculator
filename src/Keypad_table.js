da08c85415165f8235baf41a56ce46a9917376db
Victor Lazaro <lazarofilm@gmail.com>
2020-10-01 17:40:48 -0400

    finished styling keypad. checks 1 through 5

--- src/Keypad_table.js
+++ src/Keypad_table.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./Keypad_table.css";
 
 const createButton = (key) => {
-  return <button className="btn-custom">{key}</button>;
+  return <button className="keypad">{key}</button>;
 };
 
 function Keypad_table() {
@@ -11,36 +11,71 @@ function Keypad_table() {
     <>
       <table>
         <tr>
-          <td className="btn-dark-grey">{createButton(`AC`)}</td>
+          <td id="clear" className="btn-dark-grey" style={{ fontSize: "90%" }}>
+            {createButton(`AC`)}
+          </td>
           <td className="btn-dark-grey">{createButton(`%`)}</td>
-          <td className="btn-dark-grey">{createButton(`÷`)}</td>
-          <td className="btn-dark-grey">{createButton(`×`)}</td>
+          <td id="divide" className="btn-dark-grey">
+            {createButton(`÷`)}
+          </td>
+          <td id="multiply" className="btn-dark-grey">
+            {createButton(`×`)}
+          </td>
         </tr>
         <tr>
-          <td className="btn-light-grey">{createButton(`7`)}</td>
-          <td className="btn-light-grey">{createButton(`8`)}</td>
-          <td className="btn-light-grey">{createButton(`9`)}</td>
-          <td className="btn-orange">{createButton(`-`)}</td>
+          <td id="seven" className="btn-light-grey">
+            {createButton(`7`)}
+          </td>
+          <td id="eight" className="btn-light-grey">
+            {createButton(`8`)}
+          </td>
+          <td id="nine" className="btn-light-grey">
+            {createButton(`9`)}
+          </td>
+          <td id="subtract" className="btn-orange">
+            {createButton(`-`)}
+          </td>
         </tr>
         <tr>
-          <td className="btn-light-grey">{createButton(`4`)}</td>
-          <td className="btn-light-grey">{createButton(`5`)}</td>
-          <td className="btn-light-grey">{createButton(`6`)}</td>
-          <td className="btn-orange">{createButton(`+`)}</td>
+          <td id="four" className="btn-light-grey">
+            {createButton(`4`)}
+          </td>
+          <td id="five" className="btn-light-grey">
+            {createButton(`5`)}
+          </td>
+          <td id="six" className="btn-light-grey">
+            {createButton(`6`)}
+          </td>
+          <td id="add" className="btn-orange">
+            {createButton(`+`)}
+          </td>
         </tr>
         <tr>
-          <td className="btn-light-grey">{createButton(`1`)}</td>
-          <td className="btn-light-grey">{createButton(`2`)}</td>
-          <td className="btn-light-grey">{createButton(`3`)}</td>
-          <td rowspan="2" className="btn-orange">
+          <td id="one" className="btn-light-grey">
+            {createButton(`1`)}
+          </td>
+          <td id="two" className="btn-light-grey">
+            {createButton(`2`)}
+          </td>
+          <td id="three" className="btn-light-grey">
+            {createButton(`3`)}
+          </td>
+          <td
+            id="equals"
+            style={{ height: "120px" }}
+            rowspan="2"
+            className="btn-orange"
+          >
             {createButton(`=`)}
           </td>
         </tr>
         <tr>
-          <td colspan="2" className="btn-light-grey">
+          <td id="zero" colspan="2" className="btn-light-grey">
             {createButton(`0`)}
           </td>
-          <td className="btn-light-grey">{createButton(`.`)}</td>
+          <td id="decimal" className="btn-light-grey">
+            {createButton(`.`)}
+          </td>
         </tr>
       </table>
     </>
