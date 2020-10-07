44d898d21dc3e66c154cc0551311f329b0bf1631
Victor Lazaro <lazarofilm@gmail.com>
2020-10-07 15:54:54 -0400

    Updates

---
+++ precache-manifest.a381dbbe107718e9953aa5ea17ccec08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610a44f65c0738632433fb0cc047604b",
+    "url": "/fcc-calculator/index.html"
+  },
+  {
+    "revision": "eb76147c326996638d55",
+    "url": "/fcc-calculator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9db7516c3caaf83c84d7",
+    "url": "/fcc-calculator/static/css/main.8bfe23dd.chunk.css"
+  },
+  {
+    "revision": "eb76147c326996638d55",
+    "url": "/fcc-calculator/static/js/2.0f2ea686.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/fcc-calculator/static/js/2.0f2ea686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db7516c3caaf83c84d7",
+    "url": "/fcc-calculator/static/js/main.f49c608e.chunk.js"
+  },
+  {
+    "revision": "7281f81b1ccd46a2aea4",
+    "url": "/fcc-calculator/static/js/runtime-main.ea8d039d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fcc-calculator/precache-manifest.7c023c21c2d7f262800867f53350a705.js"
+  "/fcc-calculator/precache-manifest.a381dbbe107718e9953aa5ea17ccec08.js"
 );
 
 self.addEventListener('message', (event) => {
