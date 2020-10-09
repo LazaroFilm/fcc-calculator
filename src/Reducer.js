f50db23b2a0a704c220128081224b27f34a87e9c
Victor Lazaro <lazarofilm@gmail.com>
2020-10-09 10:58:35 -0400

    I give up on backspace

--- src/Reducer.js
+++ src/Reducer.js
@@ -15,7 +15,7 @@ const keys = {
   decimal: [`.`, ".", "dot"],
 
   add: [`+`, "+", "oper"],
-  subtract: [`-`, "–", "oper"],
+  subtract: [`-`, "-", "oper"],
   multiply: [`*`, "×", "oper"],
   divide: [`/`, "÷", "oper"],
 
@@ -118,18 +118,17 @@ const handleDot = (state) => {
 const handleOper = (state, id) => {
   let { calc, result, prevType } = state;
   const keySym = keys[id][0];
-  const keyName = keys[id][1];
   if (prevType === "equal") {
     return {
       calc: result + keySym,
-      result: result + keyName,
+      result: result + keySym,
       twoBack: prevType,
       prevType: "oper",
     };
   } else if (prevType === "dot") {
     return {
       calc: calc.slice(0, -1) + keySym,
-      result: result.slice(0, -1) + keyName,
+      result: result.slice(0, -1) + keySym,
       twoBack: prevType,
       prevType: "oper",
     };
@@ -137,13 +136,13 @@ const handleOper = (state, id) => {
     if (calc === "0") {
       return {
         calc: keySym,
-        result: keyName,
+        result: keySym,
         prevType: "oper",
       };
     } else {
       return {
         calc: calc + keySym,
-        result: result + keyName,
+        result: result + keySym,
         twoBack: prevType,
         prevType: "oper",
       };
@@ -151,7 +150,7 @@ const handleOper = (state, id) => {
   } else {
     return {
       calc: calc.replace(/([+\-*/]+)$/, "") + keySym,
-      result: result.replace(/([+\–×÷]+)$/, "") + keyName,
+      result: result.replace(/([+\-*/]+)$/, "") + keySym,
       twoBack: prevType,
       prevType: "oper",
     };
@@ -167,41 +166,95 @@ const handleEqual = (state) => {
   if (prevType === "dot") {
     return {
       calc: calc.slice(0, -1) + " =",
-      result: format(evaluate(calc.slice(0, -1)), { precision: 14 }),
+      result: format(evaluate(calc.slice(0, -1)), { precision: 8 }),
       twoBack: "equal",
       prevType: "equal",
     };
   }
   return {
     calc: calc + " =",
-    result: format(evaluate(calc), { precision: 14 }),
+    result: format(evaluate(calc), { precision: 8 }),
     twoBack: "equal",
     prevType: "equal",
   };
 };
 const handleBksp = (state) => {
-  let { calc, result, twoBack } = state;
-  //! needs to work with prevType num only
-  //! check that there is a prevType existing
-  //! make sure there is at least one char in calc and result
-  if (result.slice(0, -1).length <= 1) {
+  let { calc, result } = state;
+  let getType = (sym) => {
+    let type = sym;
+    for (let id in keys) {
+      type = type.replace(keys[id][0], keys[id][2]);
+    }
+    console.log(`Replace: ${type}`);
+    return type;
+  };
+  const restoreResult = (calc) => {
+    if (calc === "0") {
+      console.log("all gone");
+      return "allgone";
+    } else if (result.length <= 1) {
+      console.log("do the thing");
+      let i = calc.length - 2;
+      let restoredResult = calc[i];
+      console.log(calc[i]);
+      console.log(calc[i - 1]);
+      // chars of same type
+      while (getType(calc[i]) === getType(calc[i - 1]) && i > 0) {
+        console.log(
+          `i= ${i} -- type i= ${getType(calc[i])} -- type i-1= ${getType(
+            calc[i - 1]
+          )}`
+        );
+        restoredResult = calc[i - 1] + restoredResult;
+        console.log(`while loop: ${restoredResult}`);
+        i--;
+      }
+      console.log(`now i is: ${i}`);
+      return restoredResult;
+    } else {
+      console.log("all good");
+      return result.slice(0, -1);
+    }
+  };
+
+  if (calc.length <= 1) {
+    return { ...initialState };
+  } else if (result.length <= 2) {
+    console.log("more than 2");
     return {
-      calc: "0",
-      result: "0",
-      prevType: twoBack,
-    };
-  } else if (calc.slice(0, -1).length <= 1) {
-    return {
-      calc: "0",
-      result: "0",
-      prevType: twoBack,
+      calc: calc.slice(0, -1),
+      result: restoreResult(calc),
+      prevType: getType(calc[calc.length - 2]),
     };
   } else {
+    console.log("last resort");
     return {
       calc: calc.slice(0, -1),
       result: result.slice(0, -1),
-      prevType: twoBack,
+      prevType: getType(calc[calc.length - 2]),
     };
   }
+  //! needs to work with prevType num only
+  //! check that there is a prevType existing
+  //! make sure there is at least one char in calc and result
+  // if (result.slice(0, -1).length <= 1) {
+  //   return {
+  //     calc: "0",
+  //     result: "0",
+  //     prevType: twoBack,
+  //   };
+  // } else if (calc.slice(0, -1).length <= 1) {
+  //   return {
+  //     calc: "0",
+  //     result: "0",
+  //     prevType: twoBack,
+  //   };
+  // } else {
+  //   return {
+  //     calc: calc.slice(0, -1),
+  //     result: result.slice(0, -1),
+  //     prevType: twoBack,
+  //   };
+  // }
 };
 export { keys, initialState, reducer };
